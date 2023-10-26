4590b402e3d80ce935ed7b8eede5cb64e5d2943f
FarazRashid <muhammadfarazrashid@gmail.com>
2023-10-26 22:47:32 +0500

    Adds ternary operator
    Helps simplify code

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const items = ["Islamabad", "Lahore", "Karachi", "Quetta"];
+  let items = ["Islamabad", "Lahore", "Karachi", "Quetta"];
+  items = [];
+
+  // const message = items.length === 0 ? <p>No Items Found</p> : null;
 
   return (
     <>
       <h1>List</h1>
+      {/*items.length === 0 ? <p>No Items Found</p> : null*/}
+      {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
