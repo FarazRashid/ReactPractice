d8a73b71f1abf261d09616eb78377f22bba7392a
FarazRashid <muhammadfarazrashid@gmail.com>
2023-10-27 14:41:30 +0500

    Passes Functions Via Props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,9 +3,13 @@ import ListGroup from "./components/ListGroup";
 function App() {
   const items = ["Islamabad", "Lahore", "Karachi", "Quetta"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="List" />
+      <ListGroup items={items} heading="List" onSelectItem={handleSelectItem} />
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
@@ -24,6 +25,7 @@ function ListGroup({ items, heading }: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
