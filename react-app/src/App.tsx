f321e82e63ef91350866d49ac6e805a1b8bdd3f3
FarazRashid <muhammadfarazrashid@gmail.com>
2023-10-28 17:49:32 +0500

    Adds cart object
    Practicing with use states

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,40 @@
-import Like from "./components/Like";
-import { ReactNode } from "react";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      {
+        id: 1,
+        title: "Product 1",
+        quantity: 1,
+      },
+      {
+        id: 2,
+        title: "Product 2",
+        quantity: 3,
+      },
+    ],
+  });
+
   const handleClick = () => {
-    console.log("clicked");
+    setCart({
+      ...cart,
+      items: cart.items.map((item) => {
+        if (item.id === 1) {
+          return { ...item, quantity: 2 };
+        }
+        return item;
+      }),
+    });
   };
 
-  const div: ReactNode = (
+  return (
     <div>
-      <Like onClick={handleClick} />
+      <input></input>
+      <button>Submit</button>
     </div>
   );
-
-  return div;
 }
 
 export default App;
