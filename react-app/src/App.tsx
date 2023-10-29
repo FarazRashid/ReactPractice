c069ce84f0d83353d22dbd3a601f7a3784320afd
FarazRashid <muhammadfarazrashid@gmail.com>
2023-10-29 17:55:17 +0500

    Adds Form Component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,20 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in ornare
-        nunc, sed aliquam justo. Nulla facilisi. Duis sapien turpis, scelerisque
-        vel porttitor a, suscipit et ante. Vivamus imperdiet massa quis turpis
-        dapibus pharetra. Aenean tincidunt metus eu urna dapibus, non sagittis
-        est pellentesque. Aliquam eu erat nunc. Sed congue blandit leo eu
-        sodales. Vestibulum venenatis faucibus velit, non elementum purus
-        tincidunt imperdiet. Nulla eget rutrum quam. Vestibulum bibendum lacus
-        sit amet ipsum euismod pulvinar. Vivamus in lacinia metus, in tristique
-        urna. Vestibulum eget massa suscipit, vestibulum erat sit amet, rhoncus
-        nibh.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
