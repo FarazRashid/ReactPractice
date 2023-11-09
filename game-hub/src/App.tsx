e0f16e1c27e186c7983ad648d06aaef0cf7eb533
FarazRashid <muhammadfarazrashid@gmail.com>
2023-11-09 19:47:42 +0500

    Adds a responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,5 +1,25 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      >
+        <GridItem area={"nav"} bg={"coral"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"lightblue"}>
+            aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"gold"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
