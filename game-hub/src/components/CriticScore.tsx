5288b5aa8637d101f47325f316b1536713eef012
FarazRashid <muhammadfarazrashid@gmail.com>
2023-11-10 15:05:18 +0500

    Adds CriticScores

---
+++ game-hub/src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
+
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"15px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+}
+
+export default CriticScore;
